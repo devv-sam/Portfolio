8b71cd8bcd059b71df3b6b5c0c261177e7df0958
devsam <samuelydev@gmail.com>
2024-09-16 18:22:27 -0400

    bento grid redesign will work on extras

--- src/Components/Bento.js
+++ src/Components/Bento.js
@@ -2,51 +2,74 @@ import React, { useEffect } from 'react'
 import '../index.css';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
+import { Marquee } from "@devnomic/marquee";
+// import "@devnomic/marquee/dist/index.css";
 
 
 
 const Bento = () => {
-useEffect(() => {
+// useEffect(() => {
 
-  gsap.registerPlugin(ScrollTrigger)
+//   gsap.registerPlugin(ScrollTrigger)
 
-  const lift = document.querySelectorAll('.lift')
+//   const lift = document.querySelectorAll('.lift')
   
-  gsap.from(lift, {
-    scrollTrigger:{ 
-      trigger: lift,
-      start: 'top 80%',
-      end: 'top 20%',
-      scrub: false,
-      markers: false,
-    },
-    y: 20,            
-    opacity: 0,        
-    stagger: 0.1,     
-    duration: 1.8,       
-    ease: 'power2.out', 
+//   gsap.from(lift, {
+//     scrollTrigger:{ 
+//       trigger: lift,
+//       start: 'top 80%',
+//       end: 'top 20%',
+//       scrub: false,
+//       markers: false,
+//     },
+//     y: 20,            
+//     opacity: 0,        
+//     stagger: 0.1,     
+//     duration: 1.8,       
+//     ease: 'power2.out', 
     
-  });
-})
+//   });
+// })
 
 
   return (
     <>
-   <section className="bento-grid">
-   <div className='bx title-bx lift'>
-        <h3>About Me</h3>
+       <div className='bx'>
+       <section className="bento-grid">
+<div className='title-txt-stack'>
+<div className='title-bx lift'>
+      <div className='title-stack'>
+      <h3>About Me</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor     
         incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
         ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
 
     </div>
-    <div className='bx experience-bx lift'>
-       <div className='experience-top'> 
-        <h3>Experience</h3>
-        <a href="#" ><ion-icon name="open-outline"></ion-icon></a>
+    <div className='skillset-bx lift'>
+      <div className='skillset-stack'>
+        <h3>Stack</h3>
+        <Marquee fade={true} pauseOnHover={true}>
+            <div><img src="./assets/react.svg" alt="stack logo"></img></div>
+            <div><img src="./assets/js.svg" alt="stack logo"></img></div>
+            <div><img src="./assets/html.svg" alt="stack logo"></img></div>
+            <div><img src="./assets/css.svg" alt="stack logo"></img></div>
+            <div><img src="./assets/figma.svg" alt="stack logo"></img></div>
+            <div><img src="./assets/framer.svg" alt="stack logo"></img></div>
+          </Marquee>
+
+      </div>
+
         </div>
-        <div className='job'>
+</div>
+ 
+    <div className='experience-bx lift'>
+    <div className='experience-top'> 
+        <h3>Experience</h3>
+        <a href="#"><img src="./assets/arrow.svg"></img></a>
+        </div>
+      <div className='exp-stack'>
+      <div className='job'>
         <div className='job-desc'><h4 className='job-title'>
         Project Empower
         </h4> <div className='dash-dark'></div> <p className='role'>
@@ -55,30 +78,15 @@ useEffect(() => {
         
         <p className='duration'>
         AUG  2024  - PRESENT
-
         </p>
         <p className='desc'>Managed modern frontend projects with peers. 
         </p>
         </div>
-        <div className='job '>
-        <div className='job-desc'><h4 className='job-title'>
-        CleanLeeNess LLC
-        </h4> <div className='dash-dark'></div> <p className='role'>
-        Data management intern
-        </p></div>
-        
-        <p className='duration'>
-        FEB  2024  - MAR 2024
-
-        </p>
-        <p className='desc'>Developed skills in collecting and organizing data using Python web scraping and Google Sheets.
-        </p>
-        </div>
         <div className='job'>
         <div className='job-desc'><h4 className='job-title'>
-      DWC High School
+      DWC HS
         </h4> <div className='dash-dark'></div> <p className='role'>
-        Web Development Intern
+        Frontend Developer
         </p></div>
         
         <p className='duration'>
@@ -106,34 +114,27 @@ useEffect(() => {
         <p className='desc'>Completed coursework focused on career readiness and professional skills.</p>
 
         </div>
-      
-        
-
+      </div>
     </div>
-  
+
     
-        <div className='bx skillset-bx lift'>
-            <h3>Skillset</h3>
-            <span className='skills'>
-            <span><ion-icon name="logo-react"></ion-icon><p>React</p></span>
-            <span><ion-icon name="logo-figma"></ion-icon><p>Figma</p></span>
-            <span><ion-icon name="logo-html5"></ion-icon><p> HTML5</p></span>
-            <span><ion-icon name="logo-css3"></ion-icon><p>CSS3</p></span>
-            <span><ion-icon name="logo-javascript"></ion-icon><p className=' '>JavaScript</p></span>
-            </span>
-        </div>
-      
-      
-        <div className='bx external-bx lift'>
-          <h3>Recently played</h3>
+<div className='image-extra-stack'>
+<div className='image-bx lift'>
+    <img src="./assets/profile-image.png" alt="Sam's Profile"/>
+  </div>
+  <div className='external-bx lift'>
         <iframe src="https://open.spotify.com/embed/album/0rwbMKjNkp4ehQTwf9V2Jk?utm_source=generator" 
         width="100%" height="352" frameBorder="0" allowfullscreen="" 
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        {/* <SpotifyAuth /> */}
         </div>
+
+  </div>  
+      
 
 
    </section>
+       </div>
+  
     
   
     </>
--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -67,63 +67,60 @@ export const Landing = ({loadertext}) => {
   return (
 <>
 <Preloader loadertext={loadertext} />
-<div className="hero-image-start"></div>
-      
-        <section className='cta-opening'>
-          <Nav />
-            <div className="hero">
-              <img src="/assets/profile-icon.png" alt="Sam's profile"></img>
-               <h3 className='rvl-hd'>I'm Sam — a user interface designer & developer from New York.</h3>
-               <p className='rvl-pg' > 
-               I bring ideas to life with precision and creativity, crafting exceptional projects that stand out.
-               </p> 
+<section className='cta-opening'>
+                <Nav />
+                  <div className="hero">
+                    <img src="/assets/profile-icon.png" alt="Sam's profile"></img>
+                    <h3 className='rvl-hd'>I'm Sam — a user interface designer & developer from New York.</h3>
+                    <p className='rvl-pg' > 
+                    I bring ideas to life with precision and creativity, crafting exceptional projects that stand out.
+                    </p> 
+                      </div>
+                  <div className="base">
+                    <div className='avail '>
+                        <div className='wrap'>
+                        <span className='dot '></span>
+                        <p>Ready to collaborate </p>
+                        </div>
+                        </div>
+                    <div className='icons'>
+                        <a target="_blank" href="https://www.linkedin.com/in/samuel-yeboah-asi-82b28a2b9/" className='link-icon'><ion-icon name="logo-linkedin"></ion-icon></a>
+                        <a target="_blank" href="mailto:samuelydev@gmail.com?subject=Collaboration%20Inquiry" className='link-icon'><ion-icon name="mail-outline"></ion-icon></a>
+                        <a target="_blank" href="https://github.com/devv-sam?tab=overview&from=2024-08-01&to=2024-08-12" className='link-icon'><ion-icon name="logo-github"></ion-icon></a>
+                      </div>
+                
+                    </div>
+                              
+      </section> 
+      <section className='video-section'>
+                <div className='vid-box' ref={containerRef}>
+                        <video
+                          ref={videoRef}
+                          src="/assets/video.mp4"
+                          width="100%"
+                          height="100%"
+                          loop
+                          playsInline
+                          autoPlay
+                          muted
+                        />
                 </div>
-            <div className="base">
-               <div className='avail '>
-                   <div className='wrap'>
-                   <span className='dot '></span>
-                   <p>Ready to collaborate </p>
-                   </div>
-                  </div>
-               <div className='icons'>
-                  <a target="_blank" href="https://www.linkedin.com/in/samuel-yeboah-asi-82b28a2b9/" className='link-icon'><ion-icon name="logo-linkedin"></ion-icon></a>
-                  <a target="_blank" href="mailto:samuelydev@gmail.com?subject=Collaboration%20Inquiry" className='link-icon'><ion-icon name="mail-outline"></ion-icon></a>
-                  <a target="_blank" href="https://github.com/devv-sam?tab=overview&from=2024-08-01&to=2024-08-12" className='link-icon'><ion-icon name="logo-github"></ion-icon></a>
-                </div>
-          
-               </div>
-                        
-        </section>
-      
-          <section className='video-section'>
-          <div className='vid-box' ref={containerRef}>
-                  <video
-                    ref={videoRef}
-                    src="/assets/video.mp4"
-                    width="100%"
-                    height="100%"
-                    loop
-                    playsInline
-                    autoPlay
-                    muted
-                  />
-          </div>
-          </section>
-   
+      </section>
 
-          <section className='showcase-portfolio-intro'>
-          <div className='support'>
-                  <h3>Featured Work</h3>
-                  <p className='lt-rvl'>Dive into my showcase of innovative and meticulously crafted applications that solve real-world problems and push the boundaries of technology.</p>
-                  <a href="/portfolio" className='btn'>Case Studies</a>
-              </div>
+    <div className='column-wrap'>     
+      <section className='showcase-portfolio-intro'>
+                <div className='support'>
+                        <h3>Featured Work</h3>
+                        <p className='lt-rvl'>Dive into my showcase of innovative and meticulously crafted applications that solve real-world problems and push the boundaries of technology.</p>
+                        <a href="/portfolio" className='btn'>Case Studies</a>
+                    </div>
 
-              <PortItem video='/assets/video.mp4' name='Project Name' tag='Finance' tag2="Web Design"/>
+                    <PortItem video='/assets/video.mp4' name='Project Name' tag='Finance' tag2="Web Design"/>
 
-          </section>
-
-<Bento />
-<Footer />
+      </section>
+      <Bento />
+    </div>
+      <Footer />
     </>
   )
 }
